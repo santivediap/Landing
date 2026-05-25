4c15b8715909d9b5dcb44e4f28cb10721112c2a1
santivediap <santivediap@gmail.com>
2026-05-25 18:33:55 +0200

    remove: cloudfare dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://santiagovedia.com',
-  adapter: cloudflare(),
   integrations: [sitemap()]
 });
