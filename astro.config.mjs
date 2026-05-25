98e0ea1a0728be63618b5f5db55c2a088feeab3d
santivediap <santivediap@gmail.com>
2026-05-25 18:03:15 +0200

    add: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [sitemap()]
 });
