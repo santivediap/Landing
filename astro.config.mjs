22f363e98c9fda07637bb99d15b004e9a24d0cb2
santivediap <santivediap@gmail.com>
2026-05-25 18:38:15 +0200

    fix: restore cloudflare adapter to fix build crash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
+import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://santiagovedia.com',
+  adapter: cloudflare(),
   integrations: [sitemap()]
 });
